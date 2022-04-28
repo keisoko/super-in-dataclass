be5d75730548a1e0f7a3c16b41cd8ea82fc20ae5
Dmitriy Goldner <keisoko@gmail.com>
2022-04-28 12:31:09 -0400

    Remove auto import from Enum. Change string to reflect enum value

--- dataclass_post_init_and_super.py
+++ dataclass_post_init_and_super.py
@@ -3,14 +3,14 @@ Codecademy Intermediate Python 3 OOP lesson on super() in dataclass syntax with 
 """
 
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import Enum
 
 
 class Role(Enum):
     """Employee roles."""
 
-    WORKER = auto()
-    MANAGER = auto()
+    WORKER = "Worker"
+    MANAGER = "Manager"
 
 
 @dataclass(kw_only=True)
@@ -32,7 +32,7 @@ class Employee:
     def say_id(self) -> str:
         """Displays the employee's id and role."""
 
-        return f"my id is {self.employee_id} and my role is {self.employee_role}."
+        return f"my id is {self.employee_id} and my role is that of {self.employee_role.value}."
 
 
 @dataclass(kw_only=True)
@@ -48,22 +48,38 @@ class Manager(Employee):
 def main() -> None:
     """Main function"""
 
-    worker1 = Employee(name="Mary", age=30, employee_role=Role.WORKER,)
-    worker2 = Employee(name="John", age=35, employee_role=Role.WORKER,)
-    manager1 = Manager(name="John", age=40, employee_role=Role.MANAGER,)
-    manager2 = Manager(name="Mark", age=45, employee_role=Role.MANAGER,)
+    worker1 = Employee(
+        name="Mary",
+        age=30,
+        employee_role=Role.WORKER,
+    )
+    worker2 = Employee(
+        name="John",
+        age=35,
+        employee_role=Role.WORKER,
+    )
+    manager1 = Manager(
+        name="John",
+        age=40,
+        employee_role=Role.MANAGER,
+    )
+    manager2 = Manager(
+        name="Mark",
+        age=45,
+        employee_role=Role.MANAGER,
+    )
 
     workers = [worker1, worker2]
 
     for worker in workers:
-        print(
-            f"My name is {worker.name}, I am {worker.age} years old, {worker.say_id}")
+        print(f"My name is {worker.name}, I am {worker.age} years old, {worker.say_id}")
 
     managers = [manager1, manager2]
 
     for manager in managers:
         print(
-            f"My name is {manager.name}, I am {manager.age} years old, {manager.say_id}")
+            f"My name is {manager.name}, I am {manager.age} years old, {manager.say_id}"
+        )
 
 
 if __name__ == "__main__":
